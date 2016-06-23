230814387d29d4f37547d6f8b3b5f850a72f0426
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2016-06-22 18:08:04 -0700

    Moved remote COM creation into external comutil package

--- api/iadsopendsobject.go
+++ api/iadsopendsobject.go
@@ -1,10 +1,10 @@
 package api
 
 import (
-	"errors"
 	"unsafe"
 
 	"github.com/go-ole/go-ole"
+	"github.com/scjalliance/comutil"
 )
 
 // IADsOpenDSObjectVtbl represents the component object model virtual
@@ -28,44 +28,7 @@ func (v *IADsOpenDSObject) VTable() *IADsOpenDSObjectVtbl {
 
 // NewIADsOpenDSObject returns a new instance of the IADsOpenDSObject
 // component object model interface.
-func NewIADsOpenDSObject(server string) (ds *IADsOpenDSObject, err error) {
-	// FIXME: Right now the caller is expected to wrap this call between
-	//        CoInitialize and CoUninitialize, but maybe this function should call
-	//        them itself?
-
-	var bserver *int16
-	if len(server) > 0 {
-		bserver = ole.SysAllocStringLen(server)
-		if bserver == nil {
-			return nil, ole.NewError(ole.E_OUTOFMEMORY)
-		}
-		defer ole.SysFreeString(bserver)
-	}
-
-	serverInfo := &CoServerInfo{
-		Name: bserver,
-	}
-
-	var context uint
-	if server == "" {
-		context = ole.CLSCTX_SERVER
-	} else {
-		context = ole.CLSCTX_REMOTE_SERVER
-	}
-
-	results := make([]MultiQI, 0, 1)
-	results = append(results, MultiQI{IID: IID_IADsOpenDSObject})
-
-	// TODO: Figure out how to handle varying namespace providers
-	err = CreateInstanceEx(CLSID_LDAPNamespace, context, serverInfo, results)
-	//err = CreateInstanceEx(CLSID_WinNTNamespace, context, serverInfo, results)
-	if err != nil {
-		return nil, err
-	}
-
-	ds = (*IADsOpenDSObject)(unsafe.Pointer(results[0].Interface))
-	if ds == nil {
-		err = errors.New("Unable to create IADsOpenDSObject instance.")
-	}
-	return
+func NewIADsOpenDSObject(server string, clsid *ole.GUID) (ds *IADsOpenDSObject, err error) {
+	p, err := comutil.CreateRemoteObject(server, clsid, IID_IADsOpenDSObject)
+	return (*IADsOpenDSObject)(unsafe.Pointer(p)), err
 }
