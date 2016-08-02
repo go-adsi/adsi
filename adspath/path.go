067046a73a54e9844af2b63af2df5e083bb7f1c9
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2016-08-02 06:03:17 -0700

    New adspath parsing implementation
    * No longer relies on standard Go url library for parsing or string generation (the standard library couldn't parse serverless LDAP bindings, for example)
    * New code is not perfect and is in need of testing and refinement

--- adspath/path.go
+++ adspath/path.go
@@ -1,6 +1,8 @@
 package adspath
 
 import (
+	"bytes"
+	"errors"
 	"net/url"
 	"strings"
 )
@@ -25,43 +27,87 @@ type Path struct {
 
 // Parse will attempt to interpret the raw path provided as a string in URL
 // form.
-//
-// Internally it relies on the URL parsing capabilities of the `url` library.
 func Parse(rawpath string) (path *Path, err error) {
-	var u *url.URL
-	u, err = url.Parse(rawpath)
-	if err != nil {
-		return
+	var rest string
+
+	if rawpath == "" {
+		return nil, errors.New("empty path")
 	}
 
-	if u.User != nil {
-		return nil, &url.Error{Op: "parse", URL: rawpath, Err: ErrUserInfoNotPermitted}
-	}
+	path = new(Path)
 
-	if u.Fragment != "" {
-		return nil, &url.Error{Op: "parse", URL: rawpath, Err: ErrFragmentNotPermitted}
+	if path.Scheme, rest, err = parseScheme(rawpath); err != nil {
+		return nil, err
 	}
 
 	// Enforce correct capitalization of the scheme, because many ADSI schemes are
 	// case-sensitive.
 	//
-	// TODO: Decide whether its really appropriate to do this auto-correction.
-	switch strings.ToLower(u.Scheme) {
+	// TODO: Decide whether it's really appropriate to do this auto-correction.
+	switch strings.ToLower(path.Scheme) {
 	case "ldap":
-		u.Scheme = LDAP
+		path.Scheme = LDAP
 	case "winnt":
-		u.Scheme = WinNT
+		path.Scheme = WinNT
 	case "iis":
-		u.Scheme = IIS
+		path.Scheme = IIS
 	case "gc":
-		u.Scheme = GC
+		path.Scheme = GC
 	}
 
-	return &Path{
-		Scheme: u.Scheme,
-		Host:   u.Host,
-		Path:   u.Path,
-	}, nil
+	if rest == "" {
+		// We're binding to the root of the namespace
+		return
+	}
+
+	if !strings.HasPrefix(rest, "//") {
+		return nil, errors.New("Invalid ADS path")
+	}
+
+	rest = rest[2:]
+
+	authority, rest := split(rest, "/")
+	if rest == "" && path.Scheme == LDAP && strings.ContainsRune(authority, '=') {
+		// This is serverless LDAP binding
+		rest = authority
+		authority = ""
+	}
+
+	path.Host, path.Path = authority, rest
+
+	return
+}
+
+func parseScheme(rawpath string) (scheme, path string, err error) {
+	for i := 0; i < len(rawpath); i++ {
+		c := rawpath[i]
+		switch {
+		case 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z':
+		// do nothing
+		case '0' <= c && c <= '9' || c == '+' || c == '-' || c == '.':
+			if i == 0 {
+				return "", rawpath, nil
+			}
+		case c == ':':
+			if i == 0 {
+				return "", "", errors.New("missing protocol scheme")
+			}
+			return rawpath[:i], rawpath[i+1:], nil
+		default:
+			// we have encountered an invalid character,
+			// so there is no valid scheme
+			return "", rawpath, nil
+		}
+	}
+	return "", rawpath, nil
+}
+
+func split(input string, cut string) (string, string) {
+	i := strings.Index(input, cut)
+	if i < 0 {
+		return input, ""
+	}
+	return input[:i], input[i+len(cut):]
 }
 
 // URL converts the path to a url.URL.
@@ -75,5 +121,20 @@ func (p *Path) URL() (u *url.URL) {
 
 // String returns the path as a string in URL form.
 func (p *Path) String() string {
-	return p.URL().String()
+	var buf bytes.Buffer
+	if p.Scheme != "" {
+		buf.WriteString(p.Scheme)
+		buf.WriteByte(':')
+	}
+	if p.Scheme != "" && (p.Host != "" || p.Path != "") {
+		buf.WriteString("//")
+	}
+	if p.Host != "" {
+		buf.WriteString(p.Host)
+		if p.Path != "" {
+			buf.WriteByte('/')
+		}
+	}
+	buf.WriteString(p.Path)
+	return buf.String()
 }
