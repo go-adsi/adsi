92570040283837a701b11b4ac78e7782bc740074
Matt LaPlante <mattl@google.com>
2021-09-21 09:23:31 -0500

    Add object.ToUser and new IADsUser functions.
    Adding object.ToUser, user.AccountDisabled, user.SetAccountDisabled,
    & user.FullName.

--- api/iadsuser_stub.go
+++ api/iadsuser_stub.go
@@ -1,1 +1,21 @@
+//go:build !windows
+// +build !windows
+
 package api
+
+import "github.com/go-ole/go-ole"
+
+// AccountDisabled retrieves the disablement status of a user account.
+func (v *IADsUser) AccountDisabled() (disabled bool, err error) {
+	return false, ole.NewError(ole.E_NOTIMPL)
+}
+
+// SetAccountDisabled sets an account as disabled.
+func (v *IADsUser) SetAccountDisabled(disabled bool) (err error) {
+	return ole.NewError(ole.E_NOTIMPL)
+}
+
+// FullName returns the user's FullName property.
+func (v *IADsUser) FullName() (name string, err error) {
+	return "", ole.NewError(ole.E_NOTIMPL)
+}
